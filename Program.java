d93bf1de1fd9deec841385409e5036fe4b55dd1b
Bogdan <Bogdan.drzewiecki93@gmail.com>
2017-01-03 14:59:16 +0100

    Dodana klasa Player

--- Program.java
+++ Program.java
@@ -4,30 +4,48 @@ import java.awt.*;
 import java.awt.geom.*;
 import java.util.ArrayList;
 
+class Player extends Rectangle2D.Float
+{
+	Player(int pozX, int pozY)
+	{
+		this.x = pozX;
+		this.y = pozY;
+		this.width = 20;
+		this.height = 20;
+	}
+}
+
+
+
 public class Program extends JFrame implements KeyListener
 {
-	private int pozX = 30;
-    private int pozY = 50;
+
+	static int pozX = 50, pozY = 50;	
+	private int szerokoscOkna = 640;
+	private int wysokoscOkna = 480;
 	
 	public Program()
 	{		
-			 addKeyListener(this);
+			addKeyListener(this);
 			setResizable(false);                                                                        
             setTitle("Bomberman");                       
-            setSize(640,480);                               
+            setSize(szerokoscOkna,wysokoscOkna);                               
             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
 	}
 	
     public static void main(String[] args)                       
-    {        
+    {      
 		Program okno = new Program(); 
 		okno.setVisible(true);
     }
 	
 	public void paint(Graphics g) 
 	{
+		Player gracz = new Player(pozX, pozY);
         super.paint(g);
-        g.fillOval(pozX, pozY, 20, 20);
+		Graphics2D g2d=(Graphics2D)g;
+        g2d.fill(gracz);
+		//g.fillRect(10,10, 20, 20);
     }
 
     public void keyTyped(KeyEvent e) {        }
@@ -37,16 +55,28 @@ public class Program extends JFrame implements KeyListener
 		switch(e.getKeyChar())
 		{
                 case 'w':
-                        pozY -= 5;
-                        break;
-                case 's':
-                        pozY += 5;
+                        if ( pozY >= 40)
+						{							
+							pozY -= 5;
+						}
+						break;
+				case 's':
+                        if (pozY <= wysokoscOkna - 30)
+						{							
+							pozY += 5;
+						}
                         break;
                 case 'a':
-                        pozX -= 5;
+                        if (pozX >= 10 )
+						{
+							pozX -= 5;
+						}
                         break;
                 case 'd':
-                        pozX += 5;
+                        if (pozX <= szerokoscOkna - 30)
+						{
+							pozX += 5;
+						}
                         break;
         }
                 repaint();

308ca6e3648817c4bd76886ca6f3cc55e6d0f554
Bogdan <Bogdan.drzewiecki93@gmail.com>
2017-01-03 14:20:23 +0100

    Owal sie ruszo

--- Program.java
+++ Program.java
@@ -4,25 +4,54 @@ import java.awt.*;
 import java.awt.geom.*;
 import java.util.ArrayList;
 
-
-
-
-
-public class Program
+public class Program extends JFrame implements KeyListener
 {
-   public static void main(String[] args)                       
-   {                                                           
-      javax.swing.SwingUtilities.invokeLater(new Runnable()    
-      {                                                        
-         public void run()                                     
-         {                                                                                                                 
-            JFrame jf=new JFrame();
-			jf.setResizable(false);                                                                        
-            jf.setTitle("Bomberman");                       
-            jf.setSize(640,480);                               
-            jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-            jf.setVisible(true);                               
-         }                                                     
-      });                                                      
-   }                                                           
-}
+	private int pozX = 30;
+    private int pozY = 50;
+	
+	public Program()
+	{		
+			 addKeyListener(this);
+			setResizable(false);                                                                        
+            setTitle("Bomberman");                       
+            setSize(640,480);                               
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+	}
+	
+    public static void main(String[] args)                       
+    {        
+		Program okno = new Program(); 
+		okno.setVisible(true);
+    }
+	
+	public void paint(Graphics g) 
+	{
+        super.paint(g);
+        g.fillOval(pozX, pozY, 20, 20);
+    }
+
+    public void keyTyped(KeyEvent e) {        }
+	
+    public void keyPressed(KeyEvent e) 
+	{
+		switch(e.getKeyChar())
+		{
+                case 'w':
+                        pozY -= 5;
+                        break;
+                case 's':
+                        pozY += 5;
+                        break;
+                case 'a':
+                        pozX -= 5;
+                        break;
+                case 'd':
+                        pozX += 5;
+                        break;
+        }
+                repaint();
+    }
+    
+	public void keyReleased(KeyEvent e) {		}
+
+	}
